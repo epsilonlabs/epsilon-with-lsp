0d686be1b718a0b695b3f7c03eb683aaa0ab243e
Panagiotis Kourouklidis <panagiotis.kourouklidis@york.ac.uk>
2024-02-06 18:14:54 +0000

    Small improvements to static analyzer tests

--- plugins/org.eclipse.epsilon.eol.staticanalyser.tests/src/org/eclipse/epsilon/eol/staticanalyser/tests/EolStaticAnalyserTests.java
+++ plugins/org.eclipse.epsilon.eol.staticanalyser.tests/src/org/eclipse/epsilon/eol/staticanalyser/tests/EolStaticAnalyserTests.java
@@ -11,104 +11,116 @@ import org.eclipse.epsilon.eol.EolModule;
 import org.eclipse.epsilon.eol.staticanalyser.EolStaticAnalyser;
 import org.eclipse.epsilon.eol.staticanalyser.StaticModelFactory;
 import org.eclipse.epsilon.eol.types.EolType;
+import org.junit.BeforeClass;
+import org.junit.Ignore;
 import org.junit.Test;
 
 public class EolStaticAnalyserTests {
 	static int ident = 0;
 
+	@BeforeClass
+	public static void registerEcore() {
+		EPackage.Registry.INSTANCE.put(EcorePackage.eNS_URI, EcorePackage.eINSTANCE);
+	}
 	@Test
 	public void testModelElementTypeVariableDeclaration() throws Exception {
-		EPackage.Registry.INSTANCE.put(EcorePackage.eNS_URI, EcorePackage.eINSTANCE);
-
 		StringBuffer st = new StringBuffer();
-		st.append("model M driver EMF {nsuri=\"http://www.eclipse.org/emf/2002/Ecore\"};");
+		st.append("model M driver EMF {nsuri='http://www.eclipse.org/emf/2002/Ecore'};");
 		st.append("var i:M!EClass;");
 		assertValid(st.toString());
 	}
 	
 	@Test
 	public void testModelElementTypeVariableDeclarationError() throws Exception {
-		EPackage.Registry.INSTANCE.put(EcorePackage.eNS_URI, EcorePackage.eINSTANCE);
-
 		StringBuffer st = new StringBuffer();
-		st.append("model M driver EMF {nsuri=\"http://www.eclipse.org/emf/2002/Ecore\"};");
+		st.append("model M driver EMF {nsuri='http://www.eclipse.org/emf/2002/Ecore'};");
 		st.append("var i:M!EClss;");
-		assertValidMessage(st.toString(), "Unknown type M!EClss");
+		assertErrorMessage(st.toString(), "Unknown type M!EClss");
 	}
 	
 	@Test
 	public void testModelElementTypeAssignment() throws Exception {
-		EPackage.Registry.INSTANCE.put(EcorePackage.eNS_URI, EcorePackage.eINSTANCE);
-
 		StringBuffer st = new StringBuffer();
-		st.append("model M driver EMF {nsuri=\"http://www.eclipse.org/emf/2002/Ecore\"};");
+		st.append("model M driver EMF {nsuri='http://www.eclipse.org/emf/2002/Ecore'};");
 		st.append("var i:M!EClass = new M!EClass;");
 		assertValid(st.toString());
 	}
 	
 	@Test
 	public void testModelElementTypeAssignmentError() throws Exception {
-		EPackage.Registry.INSTANCE.put(EcorePackage.eNS_URI, EcorePackage.eINSTANCE);
-
 		StringBuffer st = new StringBuffer();
-		st.append("model M driver EMF {nsuri=\"http://www.eclipse.org/emf/2002/Ecore\"};");
+		st.append("model M driver EMF {nsuri='http://www.eclipse.org/emf/2002/Ecore'};");
 		st.append("var i:M!EClass = new M!EPackage;");
-		assertValidMessage(st.toString(), "M!EPackage cannot be assigned to M!EClass");
+		assertErrorMessage(st.toString(), "M!EPackage cannot be assigned to M!EClass");
 	}
 	
 	@Test
 	public void testModelElementTypePropertyAssignmentError() throws Exception {
-		EPackage.Registry.INSTANCE.put(EcorePackage.eNS_URI, EcorePackage.eINSTANCE);
-
 		StringBuffer st = new StringBuffer();
-		st.append("model M driver EMF {nsuri=\"http://www.eclipse.org/emf/2002/Ecore\"};");
+		st.append("model M driver EMF {nsuri='http://www.eclipse.org/emf/2002/Ecore'};");
 		st.append("var i:M!EClass = new M!EClass;");
-		st.append("i.names = \"className\";");
-		assertValidMessage(st.toString(), "Structural feature names not found in type EClass");
+		st.append("i.names = 'className';");
+		assertErrorMessage(st.toString(), "Structural feature names not found in type EClass");
+	}
+	
+	@Test
+	public void testModelElementTypePropertyAssignmentError2() throws Exception {
+		StringBuffer st = new StringBuffer();
+		st.append("model M driver EMF {nsuri='http://www.eclipse.org/emf/2002/Ecore'};");
+		st.append("var i:M!EClass = new M!EClass;");
+		st.append("i.name = 2;");
+		assertErrorMessage(st.toString(), "Integer cannot be assigned to String");
 	}
 	
 	@Test
 	public void testModelAliases() throws Exception {
-		EPackage.Registry.INSTANCE.put(EcorePackage.eNS_URI, EcorePackage.eINSTANCE);
-
 		StringBuffer st = new StringBuffer();
-		st.append("model M alias X driver EMF {nsuri=\"http://www.eclipse.org/emf/2002/Ecore\"};");
+		st.append("model M alias X driver EMF {nsuri='http://www.eclipse.org/emf/2002/Ecore'};");
 		st.append("var i:X!EClass = new X!EClass;");
-		st.append("i.name = \"className\";");
+		st.append("i.name = 'className';");
 		assertValid(st.toString());
 	}
 	
 	@Test
 	public void testTuplePropertyError() throws Exception {
 		StringBuffer st = new StringBuffer();
-		st.append("var t:Tuple = new Tuple(name = \"bob\");");
+		st.append("var t:Tuple = new Tuple(name = 'bob');");
 		st.append("t.names.println();");
-		assertValidMessage(st.toString(), "Tuple t does not have property names");
+		assertErrorMessage(st.toString(), "Tuple t does not have property names");
+	}
+	
+	
+	@Test
+	public void testNativeType() throws Exception {
+		StringBuffer st = new StringBuffer();
+		st.append("var r = new Native('java.util.Random');");
+//		st.append("r.nextint()");
+		printTree(st.toString());
+		assertValid(st.toString());
 	}
 	
 	@Test
+	@Ignore
 	public void testUseAfterDelete() throws Exception {
-		EPackage.Registry.INSTANCE.put(EcorePackage.eNS_URI, EcorePackage.eINSTANCE);
-
 		StringBuffer st = new StringBuffer();
-		st.append("model M driver EMF {nsuri=\"http://www.eclipse.org/emf/2002/Ecore\"};");
+		st.append("model M driver EMF {nsuri='http://www.eclipse.org/emf/2002/Ecore'};");
 		st.append("var i:M!EClass = new M!EClass;");
 		st.append("delete i;");
-		st.append("i.name = \"className\";");
-		assertValidMessage(st.toString(), "Cannot access object i after deletion");
+		st.append("i.name = 'className';");
+		assertErrorMessage(st.toString(), "Cannot access object i after deletion");
 	}
 	
 	@Test
+	@Ignore
 	public void testUseAfterStochasticDelete() throws Exception {
-		EPackage.Registry.INSTANCE.put(EcorePackage.eNS_URI, EcorePackage.eINSTANCE);
-
 		StringBuffer st = new StringBuffer();
-		st.append("model M driver EMF {nsuri=\"http://www.eclipse.org/emf/2002/Ecore\"};");
-		st.append("var i:M!EClass = new M!EClass;");
-		st.append("var r = new Native(\"java.util.Random\");");
-		st.append("if (r.nextint(99) > 50){delete i;}");
-		st.append("i.name = \"className\";");
-		assertValidMessage(st.toString(), "Cannot access object i after deletion");
+		st.append("model M driver EMF {nsuri='http://www.eclipse.org/emf/2002/Ecore'};\n");
+		st.append("var i:M!EClass = new M!EClass;\n");
+		st.append("var b = new Bag();\n");
+		st.append("var r = new Native('java.util.Random');\n");
+		st.append("if (r.nextint(99) > 50){delete i;}\n");
+		st.append("i.name = 'className';");
+		assertErrorMessage(st.toString(), "Cannot access object i after deletion");
 	}
 
 	@Test
@@ -123,7 +135,7 @@ public class EolStaticAnalyserTests {
 	public void testPrimitiveTypesAssignmentExpression() throws Exception {
 		StringBuffer st = new StringBuffer();
 		st.append("var i : Integer = 4;");
-		st.append("var s : String = \"test\";");
+		st.append("var s : String = 'test';");
 		st.append("(/*Integer*/i) = 5;");
 		st.append("var a : Any = true;");
 		st.append("(/*Any*/ a) = (/*String*/s);");
@@ -145,10 +157,9 @@ public class EolStaticAnalyserTests {
 	public void testPrimitiveTypesAssignmentExpressionErrorMessage() throws Exception {
 		StringBuffer st = new StringBuffer();
 		st.append("var i : Integer = 4;");
-		st.append("var s : String = \"test\";");
-		String expectedMessage = "String cannot be assigned to Integer";
+		st.append("var s : String = 'test';");
 		st.append("i = s;");
-		assertValidMessage(st.toString(), expectedMessage);
+		assertErrorMessage(st.toString(), "String cannot be assigned to Integer");
 	}
 
 	public void assertValid(String eol) throws Exception {
@@ -160,7 +171,7 @@ public class EolStaticAnalyserTests {
 		visit(module.getChildren());
 	}
 
-	public void assertValidMessage(String eol, String message) throws Exception {
+	public void assertErrorMessage(String eol, String message) throws Exception {
 		EolModule module = new EolModule();
 		module.parse(eol);
 		EolStaticAnalyser staticAnalyser = new EolStaticAnalyser(new StaticModelFactory());
@@ -190,5 +201,12 @@ public class EolStaticAnalyserTests {
 			printTree(m);
 		}
 		ident -= 1;
+	}
+	
+	public void printTree (String eol) throws Exception {
+		EolModule module = new EolModule();
+		module.parse(eol);
+		printTree(module.getChildren().get(0));
+		
 	}
 }

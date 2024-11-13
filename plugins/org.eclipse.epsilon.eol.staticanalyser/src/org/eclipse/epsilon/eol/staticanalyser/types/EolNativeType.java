bc25b65efeebda5be52e05d16259d0057b9e8ef7
Panagiotis Kourouklidis <22411884+pkourouklidis@users.noreply.github.com>
2024-11-13 13:16:31 +0000

    Support EolNativeType and refactor isCompatible

---
+++ plugins/org.eclipse.epsilon.eol.staticanalyser/src/org/eclipse/epsilon/eol/staticanalyser/types/EolNativeType.java
@@ -0,0 +1,34 @@
+package org.eclipse.epsilon.eol.staticanalyser.types;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+public class EolNativeType extends EolType {
+
+	private Class<?> javaClass;
+	
+	public EolNativeType(Class<?> javaClass) {
+		this.javaClass = javaClass;
+	}
+	
+	@Override
+	public String getName() {
+		return "Native";
+	}
+	
+	@Override
+	public String toString() {
+		return "Native<" + javaClass.getName() +">";
+	}
+	
+	@Override
+	public List<EolType> getParentTypes() {
+		if (javaClass == Object.class) {
+			return Collections.emptyList();
+		}else {
+			return Arrays.asList(new EolNativeType(javaClass.getSuperclass()));
+		}
+	}
+
+}

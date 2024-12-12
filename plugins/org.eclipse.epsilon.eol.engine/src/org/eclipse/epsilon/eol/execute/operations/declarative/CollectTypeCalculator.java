55438d209b59b539f0a29c3b78f78cf9919a224b
Panagiotis Kourouklidis <22411884+pkourouklidis@users.noreply.github.com>
2024-12-12 12:09:51 +0000

    Add type calculator for collect operation

---
+++ plugins/org.eclipse.epsilon.eol.engine/src/org/eclipse/epsilon/eol/execute/operations/declarative/CollectTypeCalculator.java
@@ -0,0 +1,15 @@
+package org.eclipse.epsilon.eol.execute.operations.declarative;
+
+import org.eclipse.epsilon.eol.execute.operations.ITypeCalculator;
+import org.eclipse.epsilon.eol.staticanalyser.types.EolCollectionType;
+import org.eclipse.epsilon.eol.staticanalyser.types.EolType;
+
+public class CollectTypeCalculator implements ITypeCalculator {
+
+	@Override
+	public EolType calculateType(EolType contextType, EolType expressionType) {
+		String collectionName = ((EolCollectionType)contextType).getName();
+		return new EolCollectionType(collectionName, expressionType);
+	}
+
+}

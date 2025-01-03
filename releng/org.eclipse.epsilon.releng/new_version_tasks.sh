ee206d5601f5620dcfab9e8a4e583ce01c3b2c32
Antonio Garcia-Dominguez <agarcdomi@gmail.com>
2025-01-03 11:15:57 +0100

    Bump new_version_tasks.sh after 2.6 release

--- releng/org.eclipse.epsilon.releng/new_version_tasks.sh
+++ releng/org.eclipse.epsilon.releng/new_version_tasks.sh
@@ -6,8 +6,8 @@ UpdatesName=updates
 JavadocsName=javadoc
 StableName=stable
 InterimVersion=interim
-NewVersion=2.6
-OldVersion=2.5
+NewVersion=2.7
+OldVersion=2.6
 InterimJavadocs=$InterimVersion-$JavadocsName
 StableJavadocs=$StableName-$JavadocsName
 
@@ -31,7 +31,7 @@ rm -rf $StableJavadocs/$InterimJavadocs &&
 echo "Adding $NewVersion to composite..." &&
 cd $Downloads/$UpdatesName &&
 export JAVA_HOME=/shared/common/jdk1.8.0_x64-latest
-/shared/common/apache-ant-latest/bin/ant -f /shared/modeling/tools/promotion/manage-composite.xml add -Dchild.repository=$NewVersion
+/shared/common/apache-ant-latest/bin/ant -f /shared/modeling.deletemeafter013025/tools/promotion/manage-composite.xml add -Dchild.repository=$NewVersion
 
 #cd /home/data/httpd/download.eclipse.org/epsilon/temp
 #curl -o epsilon-${NewVersion}-signed.zip -F file=@epsilon-${NewVersion}-unsigned.zip http://build.eclipse.org:31338/macsign.php

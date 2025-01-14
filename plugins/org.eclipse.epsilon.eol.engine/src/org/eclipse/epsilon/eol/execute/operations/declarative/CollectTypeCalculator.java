02f6a047efa46533cbf585b98a2ee465519fce73
Panagiotis Kourouklidis <22411884+pkourouklidis@users.noreply.github.com>
2025-01-14 11:30:04 +0000

    fix collect type calculator to correctly handle all collection types

--- plugins/org.eclipse.epsilon.eol.engine/src/org/eclipse/epsilon/eol/execute/operations/declarative/CollectTypeCalculator.java
+++ plugins/org.eclipse.epsilon.eol.engine/src/org/eclipse/epsilon/eol/execute/operations/declarative/CollectTypeCalculator.java
@@ -9,7 +9,17 @@ public class CollectTypeCalculator implements ITypeCalculator {
 	@Override
 	public EolType calculateType(EolType contextType, EolType iteratorType, EolType expressionType) {
 		String collectionName = ((EolCollectionType)contextType).getName();
-		return new EolCollectionType(collectionName, expressionType);
+		String newCollectionName = null;
+		if (collectionName.equals("Bag") || collectionName.equals("Set")) {
+			newCollectionName = "Bag";
+		}
+		else if (collectionName.equals("Sequence") || collectionName.equals("OrderedSet")){
+			newCollectionName = "Sequence";
+		}
+		else {
+			throw new RuntimeException("Unknown collection name");
+		}
+		return new EolCollectionType(newCollectionName, expressionType);
 	}
 
 }

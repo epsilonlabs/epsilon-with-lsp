0373b60101d20f69dfcf503e901262fdcdc7d08d
Panagiotis Kourouklidis <22411884+pkourouklidis@users.noreply.github.com>
2025-01-17 11:37:22 +0000

    add type calculator for sortBy operation

---
+++ plugins/org.eclipse.epsilon.eol.engine/src/org/eclipse/epsilon/eol/execute/operations/declarative/SortByTypeCalculator.java
@@ -0,0 +1,14 @@
+package org.eclipse.epsilon.eol.execute.operations.declarative;
+
+import org.eclipse.epsilon.eol.execute.operations.ITypeCalculator;
+import org.eclipse.epsilon.eol.staticanalyser.types.EolCollectionType;
+import org.eclipse.epsilon.eol.staticanalyser.types.EolType;
+
+public class SortByTypeCalculator implements ITypeCalculator {
+
+	@Override
+	public EolType calculateType(EolType contextType, EolType iteratorType, EolType expressionType) {
+		return new EolCollectionType("Sequence", iteratorType);
+	}
+
+}

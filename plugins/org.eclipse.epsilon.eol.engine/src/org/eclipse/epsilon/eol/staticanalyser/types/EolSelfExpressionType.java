76a75456119eb5e4eef693731beeb6f2673b091c
Panagiotis Kourouklidis <22411884+pkourouklidis@users.noreply.github.com>
2025-05-14 18:26:41 +0100

    Remove unused EolPseudotypes

--- plugins/org.eclipse.epsilon.eol.engine/src/org/eclipse/epsilon/eol/staticanalyser/types/EolSelfExpressionType.java
+++
@@ -1,7 +0,0 @@
-package org.eclipse.epsilon.eol.staticanalyser.types;
-
-public class EolSelfExpressionType extends EolPseudotype {
-
-	public static EolSelfExpressionType Instance = new EolSelfExpressionType();
-
-}

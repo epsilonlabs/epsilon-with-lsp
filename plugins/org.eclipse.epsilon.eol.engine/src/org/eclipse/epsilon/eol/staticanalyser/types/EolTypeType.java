73e498e8345e4427838f3ff43c774523669b5417
Panagiotis Kourouklidis <22411884+pkourouklidis@users.noreply.github.com>
2025-05-15 17:47:35 +0100

    Use EolTypeType in the NameExpression visitor

--- plugins/org.eclipse.epsilon.eol.engine/src/org/eclipse/epsilon/eol/staticanalyser/types/EolTypeType.java
+++ plugins/org.eclipse.epsilon.eol.engine/src/org/eclipse/epsilon/eol/staticanalyser/types/EolTypeType.java
@@ -3,13 +3,17 @@ package org.eclipse.epsilon.eol.staticanalyser.types;
 public class EolTypeType extends EolType {
 	private EolType wrappedType;
 
-	EolTypeType(EolType wrappedType){
+	public EolTypeType(EolType wrappedType){
 		this.wrappedType = wrappedType;
 	}
 	
 	@Override
 	public String getName() {
 		return "EolTypeType<" + wrappedType.getName() + ">";
+	}
+	
+	public EolType getWrappedType() {
+		return this.wrappedType;
 	}
 
 }

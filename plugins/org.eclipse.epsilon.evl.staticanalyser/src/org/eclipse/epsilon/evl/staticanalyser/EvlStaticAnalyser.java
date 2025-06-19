e2e8f223f3515f69f062915a64a323f4a2654d43
Panagiotis Kourouklidis <panagiotis.kourouklidis@gmail.com>
2025-06-19 10:56:03 +0100

    Remove unnecessary comments

--- plugins/org.eclipse.epsilon.evl.staticanalyser/src/org/eclipse/epsilon/evl/staticanalyser/EvlStaticAnalyser.java
+++ plugins/org.eclipse.epsilon.evl.staticanalyser/src/org/eclipse/epsilon/evl/staticanalyser/EvlStaticAnalyser.java
@@ -18,33 +18,34 @@ import org.eclipse.epsilon.evl.dom.IEvlVisitor;
 public class EvlStaticAnalyser extends EolStaticAnalyser implements IEvlVisitor {
 
 	public EvlStaticAnalyser() {
-		
+
 	}
-	
+
 	@Override
 	public List<ModuleMarker> validate(IModule imodule) {
-		
+
 		if (!(imodule instanceof EvlModule))
 			return Collections.emptyList();
-	
+
 		EvlModule evlModule = (EvlModule) imodule;
-		
+
 		super.preValidate(evlModule);
 		for (Pre pre : evlModule.getPre()) {
-			 pre.accept(this);
+			pre.accept(this);
 		}
 		super.mainValidate();
-		
+
 		for (ConstraintContext cc : evlModule.getConstraintContexts()) {
 			cc.accept(this);
 		}
 		for (Post post : evlModule.getPost()) {
-			 post.accept(this);
+			post.accept(this);
 		}
 		super.postValidate();
-		
+
 		return errors;
 	}
+
 	@Override
 	public void visit(Post post) {
 		post.getBody().accept(this);
@@ -57,54 +58,38 @@ public class EvlStaticAnalyser extends EolStaticAnalyser implements IEvlVisitor 
 
 	@Override
 	public void visit(ConstraintContext constraintContext) {
-		// TODO Auto-generated method stub
-		
-		//typeExpression.compile(context);
-		 constraintContext.getTypeExpression().accept(this);
-		
-		 for (Constraint c : constraintContext.getConstraints())
-			 
-			 //c.compile(context);
-			 c.accept(this);
+		constraintContext.getTypeExpression().accept(this);
+
+		for (Constraint c : constraintContext.getConstraints())
+			c.accept(this);
 	}
 
 	@Override
 	public void visit(Constraint constraint) {
-		// TODO Auto-generated method stub
-		ConstraintContext cc = (ConstraintContext)constraint.getParent();
-		context.getFrameStack().put(new Variable("self",getResolvedType(cc.getTypeExpression())));
-		 
-			
-		if (constraint.getGuardBlock()!=null)
-		//	guardBlock.compile(context);
+		ConstraintContext cc = (ConstraintContext) constraint.getParent();
+		context.getFrameStack().put(new Variable("self", getResolvedType(cc.getTypeExpression())));
+
+		if (constraint.getGuardBlock() != null)
 			constraint.getGuardBlock().accept(this);
-		
-		if (constraint.getCheckBlock()!=null)
-			//checkBlock.compile(context);
+
+		if (constraint.getCheckBlock() != null)
 			constraint.getCheckBlock().accept(this);
-		
-		if (constraint.getMessageBlock()!=null)
-		//	messageBlock.compile(context);
+
+		if (constraint.getMessageBlock() != null)
 			constraint.getMessageBlock().accept(this);
-		
-		for (Fix f : constraint.getFixes())
-		{
-			if (f.getBodyBlock()!=null)
-			//f.bodyBlock.compile(context);
+
+		for (Fix f : constraint.getFixes()) {
+			if (f.getBodyBlock() != null)
 				f.getBodyBlock().accept(this);
-			if (f.getGuardBlock()!=null)
-			//f.guardBlock.compile(context);
+			if (f.getGuardBlock() != null)
 				f.getGuardBlock().accept(this);
-			if (f.getTitleBlock()!=null)
-			//f.titleBlock.compile(context); // Is it necessary?
+			if (f.getTitleBlock() != null)
 				f.getTitleBlock().accept(this);
-		}		
+		}
 	}
-	
 
 	@Override
 	public void visit(Fix fix) {
 		// TODO Auto-generated method stub
-		
 	}
 }

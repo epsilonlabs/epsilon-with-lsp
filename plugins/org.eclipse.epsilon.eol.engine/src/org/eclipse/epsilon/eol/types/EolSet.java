4343b8c9bf1575275b477927a3c892a8f3d09f49
Antonio Garcia-Dominguez <agarcdomi@gmail.com>
2025-08-06 11:12:38 +0100

    EolSet: use LinkedHashSet to preserve insertion order

--- plugins/org.eclipse.epsilon.eol.engine/src/org/eclipse/epsilon/eol/types/EolSet.java
+++ plugins/org.eclipse.epsilon.eol.engine/src/org/eclipse/epsilon/eol/types/EolSet.java
@@ -1,22 +1,23 @@
 /*******************************************************************************
- * Copyright (c) 2012-2020 The University of York, Antonio García-Domínguez.
+ * Copyright (c) 2012-2025 The University of York, Antonio García-Domínguez.
  * This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License 2.0
  * which is available at https://www.eclipse.org/legal/epl-2.0/
  * 
  * Contributors:
  *     Dimitrios Kolovos - initial API and implementation
- *     Antonio García-Domínguez - add type parameter
+ *     Antonio García-Domínguez - add type parameter, change to LinkedHashSet
  *     Sina Madani - extends {@link EolCollection}
  ******************************************************************************/
 package org.eclipse.epsilon.eol.types;
 
+import java.util.LinkedHashSet;
 import java.util.Set;
 
 @SuppressWarnings("unchecked")
 public class EolSet<T> extends EolCollection<T> implements Set<T> {
 
 	public EolSet() {
-		super(new java.util.HashSet<>());
+		super(new LinkedHashSet<>());
 	}
 }

35be159d392693fa7a74b6ac0513d47228b27178
Panagiotis Kourouklidis <panagiotis.kourouklidis@gmail.com>
2025-08-26 17:26:06 +0100

    Asynchronously enumerate EOL files in the workspace after server init

--- plugins/org.eclipse.epsilon.lsp/src/org/eclipse/epsilon/lsp/EpsilonLanguageServer.java
+++ plugins/org.eclipse.epsilon.lsp/src/org/eclipse/epsilon/lsp/EpsilonLanguageServer.java
@@ -9,14 +9,25 @@
  *******************************************************************************/
 package org.eclipse.epsilon.lsp;
 
+import java.io.IOException;
+import java.net.URI;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
 import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.function.Consumer;
+import java.util.stream.Stream;
 
 import org.eclipse.lsp4j.InitializeParams;
 import org.eclipse.lsp4j.InitializeResult;
+import org.eclipse.lsp4j.InitializedParams;
 import org.eclipse.lsp4j.ServerCapabilities;
 import org.eclipse.lsp4j.TextDocumentSyncKind;
+import org.eclipse.lsp4j.WorkspaceFolder;
 import org.eclipse.lsp4j.services.LanguageClient;
 import org.eclipse.lsp4j.services.LanguageServer;
 import org.eclipse.lsp4j.services.TextDocumentService;
@@ -31,7 +42,10 @@ public class EpsilonLanguageServer implements LanguageServer {
     protected AtomicBoolean shutdown = new AtomicBoolean(false);
     protected Consumer<Integer> exitFunction = System::exit;
     protected LanguageClient client;
-
+    
+    private List<WorkspaceFolder> workspaceFolders;
+    private final ExecutorService executor = Executors.newSingleThreadExecutor();
+    
     public void connect(LanguageClient remoteProxy) {
         this.client = remoteProxy;
     }
@@ -60,9 +74,32 @@ public class EpsilonLanguageServer implements LanguageServer {
 
 	@Override
     public CompletableFuture<InitializeResult> initialize(InitializeParams params) {
+		workspaceFolders = params.getWorkspaceFolders();
+		if (workspaceFolders != null) ePackageRegistryManager.initialize(params.getWorkspaceFolders());
         final InitializeResult res = new InitializeResult(new ServerCapabilities());
         res.getCapabilities().setTextDocumentSync(TextDocumentSyncKind.Full);
         return CompletableFuture.completedFuture(res);
+    }
+	
+    @Override
+    public void initialized(InitializedParams params) {
+        // Schedule background analysis after client is ready
+        executor.submit(this::analyzeWorkspace);
+    }
+	
+    private void analyzeWorkspace() {
+        if (workspaceFolders == null) return;
+
+        for(WorkspaceFolder wf : workspaceFolders) {
+        	Path p  = Paths.get(URI.create(wf.getUri()));
+            try (Stream<Path> stream = Files.walk(p)) {
+                stream.filter(Files::isRegularFile)
+                      .filter(f -> f.toString().endsWith(".eol"))
+                      .forEach(f -> System.out.println(f.getFileName()));
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
     }
 
     @Override

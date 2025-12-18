35b2772d41057808aee083d3275101572806a2bd
Antonio Garcia-Dominguez <agarcdomi@gmail.com>
2025-12-18 13:16:47 +0100

    Add EVL static analyser tests to CI

--- tests/org.eclipse.epsilon.eol.staticanalyser.tests/src/org/eclipse/epsilon/eol/staticanalyser/tests/StaticAnalyserTestSuite.java
+++ tests/org.eclipse.epsilon.eol.staticanalyser.tests/src/org/eclipse/epsilon/eol/staticanalyser/tests/StaticAnalyserTestSuite.java
@@ -7,7 +7,8 @@ import org.junit.runners.Suite.SuiteClasses;
 @RunWith(Suite.class)
 @SuiteClasses({
 	BuildTest.class,
-	EolStaticAnalyserTests.class
+	EolStaticAnalyserTests.class,
+	EvlStaticAnalyserTests.class
 })
 public class StaticAnalyserTestSuite {
 

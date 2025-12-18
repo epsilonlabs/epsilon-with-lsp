d143030d694d893031250f6a636b563bee531edd
Antonio Garcia-Dominguez <agarcdomi@gmail.com>
2025-12-18 11:13:38 +0100

    Minor reformatting

--- tests/org.eclipse.epsilon.eol.staticanalyser.tests/src/org/eclipse/epsilon/eol/staticanalyser/tests/StaticAnalyserTestSuite.java
+++ tests/org.eclipse.epsilon.eol.staticanalyser.tests/src/org/eclipse/epsilon/eol/staticanalyser/tests/StaticAnalyserTestSuite.java
@@ -1,17 +1,14 @@
 package org.eclipse.epsilon.eol.staticanalyser.tests;
 
-
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 
 @RunWith(Suite.class)
 @SuiteClasses({
-		BuildTest.class,
-		EolStaticAnalyserTests.class
-		})
-
-
+	BuildTest.class,
+	EolStaticAnalyserTests.class
+})
 public class StaticAnalyserTestSuite {
 
 }

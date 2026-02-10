1ef71d835fba50204cd65822a1962710b9a1e4b7
Owen Reynolds <122989777+OwenR-York@users.noreply.github.com>
2026-02-10 17:44:53 +0000

    Migrate EOL static analyser tests to new data-driven structure (#24)
    This commit migrates the .eol scripts that were mixed with the Java source code to the new structure based on the resources folder. The new structure supports arbitrary nesting of folders, to help group related scripts together.
    
    The commit adds region information to a number of tests as well. We will be adding region information to more tests as we go along.
    
    ---------
    
    Co-authored-by: Panagiotis Kourouklidis <panagiotis.kourouklidis@gmail.com>

--- tests/org.eclipse.epsilon.eol.staticanalyser.tests/src/org/eclipse/epsilon/eol/staticanalyser/tests/StaticAnalyserTestSuite.java
+++ tests/org.eclipse.epsilon.eol.staticanalyser.tests/src/org/eclipse/epsilon/eol/staticanalyser/tests/StaticAnalyserTestSuite.java
@@ -7,7 +7,6 @@ import org.junit.runners.Suite.SuiteClasses;
 @RunWith(Suite.class)
 @SuiteClasses({
 	EolBuildTests.class,
-	EolStaticAnalyserTests.class,
 	EvlStaticAnalyserTests.class
 })
 public class StaticAnalyserTestSuite {

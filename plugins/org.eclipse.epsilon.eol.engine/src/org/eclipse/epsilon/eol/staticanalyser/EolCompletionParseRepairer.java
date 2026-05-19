abac1e0ddd829ee845bc8b3b273271f19a5eb672
Panagiotis Kourouklidis <panagiotis.kourouklidis@gmail.com>
2026-05-19 10:30:31 +0100

    Fix operation completions in the middle of file

--- plugins/org.eclipse.epsilon.eol.engine/src/org/eclipse/epsilon/eol/staticanalyser/EolCompletionParseRepairer.java
+++ plugins/org.eclipse.epsilon.eol.engine/src/org/eclipse/epsilon/eol/staticanalyser/EolCompletionParseRepairer.java
@@ -23,6 +23,9 @@ public class EolCompletionParseRepairer {
 		if (needsPlaceholder(before)) {
 			repaired.append(PLACEHOLDER);
 		}
+		if (needsInlineSemicolon(before, after)) {
+			repaired.append(';');
+		}
 		repaired.append(after);
 		appendMissingDelimiters(repaired);
 		appendMissingSemicolon(repaired);
@@ -70,6 +73,108 @@ public class EolCompletionParseRepairer {
 			}
 		}
 		return -1;
+	}
+
+	protected boolean needsInlineSemicolon(String beforeCursor, String afterCursor) {
+		int index = previousNonWhitespaceIndex(beforeCursor);
+		if (index < 0) {
+			return false;
+		}
+
+		char previous = beforeCursor.charAt(index);
+		return canCompleteStatementAtCursor(previous) && nextSignificantTokenStartsOnLaterLine(afterCursor);
+	}
+
+	protected boolean canCompleteStatementAtCursor(char previous) {
+		return previous == '.'
+			|| previous == ')'
+			|| previous == ']'
+			|| previous == '\''
+			|| previous == '"'
+			|| Character.isJavaIdentifierPart(previous);
+	}
+
+	protected boolean nextSignificantTokenStartsOnLaterLine(String text) {
+		boolean seenNewline = false;
+		for (int i = 0; i < text.length();) {
+			char c = text.charAt(i);
+			if (Character.isWhitespace(c)) {
+				seenNewline = seenNewline || isLineBreak(c);
+				i++;
+				continue;
+			}
+
+			if (startsWith(text, i, "/*")) {
+				int end = text.indexOf("*/", i + 2);
+				if (end < 0) {
+					return false;
+				}
+				seenNewline = seenNewline || containsLineBreak(text, i + 2, end);
+				i = end + 2;
+				continue;
+			}
+
+			if (startsWith(text, i, "//")) {
+				int end = nextLineBreak(text, i + 2);
+				if (end < 0) {
+					return false;
+				}
+				seenNewline = true;
+				i = end;
+				continue;
+			}
+
+			return seenNewline && canStartFollowingStatement(c);
+		}
+		return false;
+	}
+
+	protected boolean startsWith(String text, int index, String prefix) {
+		return text.regionMatches(index, prefix, 0, prefix.length());
+	}
+
+	protected int nextLineBreak(String text, int fromIndex) {
+		for (int i = fromIndex; i < text.length(); i++) {
+			if (isLineBreak(text.charAt(i))) {
+				return i;
+			}
+		}
+		return -1;
+	}
+
+	protected boolean containsLineBreak(String text, int start, int end) {
+		for (int i = start; i < end; i++) {
+			if (isLineBreak(text.charAt(i))) {
+				return true;
+			}
+		}
+		return false;
+	}
+
+	protected boolean isLineBreak(char c) {
+		return c == '\n' || c == '\r';
+	}
+
+	protected boolean canStartFollowingStatement(char c) {
+		switch (c) {
+			case ';':
+			case ')':
+			case ']':
+			case '}':
+			case '.':
+			case ',':
+			case '+':
+			case '-':
+			case '*':
+			case '/':
+			case '|':
+			case '?':
+			case ':':
+			case '=':
+				return false;
+			default:
+				return true;
+		}
 	}
 
 	protected void appendMissingDelimiters(StringBuilder repaired) {

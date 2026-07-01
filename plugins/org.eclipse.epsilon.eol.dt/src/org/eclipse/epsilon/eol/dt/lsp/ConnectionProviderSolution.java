72d86918f02715eda448f9257798cd3eb6e8d1fc
Panagiotis Kourouklidis <panagiotis.kourouklidis@gmail.com>
2026-07-01 10:59:05 +0100

    Add support for tools contributed via the extension point

--- plugins/org.eclipse.epsilon.eol.dt/src/org/eclipse/epsilon/eol/dt/lsp/ConnectionProviderSolution.java
+++ plugins/org.eclipse.epsilon.eol.dt/src/org/eclipse/epsilon/eol/dt/lsp/ConnectionProviderSolution.java
@@ -3,13 +3,21 @@ package org.eclipse.epsilon.eol.dt.lsp;
 import java.io.IOException;
 
 
+import org.eclipse.core.runtime.Platform;
+import org.eclipse.epsilon.common.dt.launching.extensions.ToolExtension;
 import org.eclipse.epsilon.eol.dt.EolPlugin;
 import org.eclipse.epsilon.lsp.EpsilonLanguageServer;
 import org.eclipse.jface.preference.IPreferenceStore;
 import org.eclipse.jface.util.IPropertyChangeListener;
+import org.osgi.framework.Bundle;
 
 public class ConnectionProviderSolution extends AbstractConnectionProvider {
-    private static final EpsilonLanguageServer LANGUAGE_SERVER = new EpsilonLanguageServer();
+    private static final EpsilonLanguageServer LANGUAGE_SERVER = new EpsilonLanguageServer() {
+        @Override
+        public synchronized ClassLoader getNativeTypeClassLoader() {
+            return new ExtensionPointToolClassLoader(super.getNativeTypeClassLoader());
+        }
+    };
     private static boolean preferenceListenerRegistered = false;
 
     public ConnectionProviderSolution() {
@@ -49,5 +57,26 @@ public class ConnectionProviderSolution extends AbstractConnectionProvider {
         }
         return EolPlugin.getDefault().getPreferenceStore()
             .getString(LspNativeTypeClasspathPreferencePage.NATIVE_TYPE_CLASSPATH);
+    }
+
+    private static class ExtensionPointToolClassLoader extends ClassLoader {
+        public ExtensionPointToolClassLoader(ClassLoader parent) {
+            super(parent);
+        }
+
+        @Override
+        protected Class<?> findClass(String name) throws ClassNotFoundException {
+            ToolExtension extension = ToolExtension.forClass(name);
+            if (extension == null) {
+                throw new ClassNotFoundException(name);
+            }
+
+            Bundle bundle = Platform.getBundle(extension.getConfigurationElement().getContributor().getName());
+            if (bundle == null) {
+                throw new ClassNotFoundException(name);
+            }
+
+            return bundle.loadClass(extension.getClazz());
+        }
     }
 }

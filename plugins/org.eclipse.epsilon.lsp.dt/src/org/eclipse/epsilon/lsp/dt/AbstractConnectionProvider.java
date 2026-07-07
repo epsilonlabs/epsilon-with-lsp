1331cf61b807848e0caf9e0dceedb607d20009e5
panagiotis <panagiotis@hp>
2026-07-07 13:02:28 +0300

    Move eclipse-specific LSP configuration into separate plugin

--- plugins/org.eclipse.epsilon.lsp.dt/src/org/eclipse/epsilon/lsp/dt/AbstractConnectionProvider.java
+++ plugins/org.eclipse.epsilon.lsp.dt/src/org/eclipse/epsilon/lsp/dt/AbstractConnectionProvider.java
@@ -1,5 +1,7 @@
-package org.eclipse.epsilon.eol.dt.lsp;
+package org.eclipse.epsilon.lsp.dt;
 
+import java.io.ByteArrayInputStream;
+import java.io.Closeable;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
@@ -9,8 +11,6 @@ import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.concurrent.Future;
-import java.io.ByteArrayInputStream;
-import java.io.Closeable;
 
 import org.eclipse.lsp4e.server.StreamConnectionProvider;
 import org.eclipse.lsp4j.jsonrpc.Launcher;
@@ -20,28 +20,27 @@ import org.eclipse.lsp4j.services.LanguageServer;
 
 public class AbstractConnectionProvider implements StreamConnectionProvider {
 
-	private InputStream clientInputStream  ;
+	private InputStream clientInputStream;
 	private OutputStream clientOutputStream;
-    private LanguageServer languageServer;
-    protected Launcher<LanguageClient> launcher;
-    private InputStream errorStream;
+	private LanguageServer languageServer;
+	protected Launcher<LanguageClient> launcher;
+	private InputStream errorStream;
 	private Future<Void> listener;
 	private Collection<Closeable> streams = new ArrayList<>(4);
 
-    public AbstractConnectionProvider(LanguageServer languageServer) {
-        this.languageServer = languageServer;
-    }
+	public AbstractConnectionProvider(LanguageServer languageServer) {
+		this.languageServer = languageServer;
+	}
 
-    @Override
-    public void start() throws IOException {
+	@Override
+	public void start() throws IOException {
 		Pipe serverOutputToClientInput = Pipe.open();
 		Pipe clientOutputToServerInput = Pipe.open();
 
 		errorStream = new ByteArrayInputStream("Error output on console".getBytes(StandardCharsets.UTF_8));
 		InputStream serverInputStream = Channels.newInputStream(clientOutputToServerInput.source());
 		OutputStream serverOutputStream = Channels.newOutputStream(serverOutputToClientInput.sink());
-		launcher = LSPLauncher.createServerLauncher(languageServer, serverInputStream,
-				serverOutputStream);
+		launcher = LSPLauncher.createServerLauncher(languageServer, serverInputStream, serverOutputStream);
 		clientInputStream = Channels.newInputStream(serverOutputToClientInput.source());
 		clientOutputStream = Channels.newOutputStream(clientOutputToServerInput.sink());
 		listener = launcher.startListening();
@@ -50,35 +49,35 @@ public class AbstractConnectionProvider implements StreamConnectionProvider {
 		streams.add(serverInputStream);
 		streams.add(serverOutputStream);
 		streams.add(errorStream);
-		
-    }
+	}
 
-    @Override
-    public InputStream getInputStream() {
-    	return clientInputStream;
-    }
+	@Override
+	public InputStream getInputStream() {
+		return clientInputStream;
+	}
 
-    @Override
-    public OutputStream getOutputStream() {
+	@Override
+	public OutputStream getOutputStream() {
 		return clientOutputStream;
-    }
+	}
 
-    @Override
-    public void stop() {
-		streams.forEach(t -> {
+	@Override
+	public void stop() {
+		streams.forEach(stream -> {
 			try {
-				t.close();
-			} catch (IOException e) {
+				stream.close();
+			}
+			catch (IOException e) {
 				e.printStackTrace();
 			}
 		});
 		streams.clear();
 		listener.cancel(true);
 		listener = null;
-    }
+	}
 
-    @Override
-    public InputStream getErrorStream() {
-        return errorStream;
-    }
+	@Override
+	public InputStream getErrorStream() {
+		return errorStream;
+	}
 }

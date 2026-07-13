7fdc72bc3f7b63d698df5f28675906552e1f8691
Panagiotis Kourouklidis <panagiotis.kourouklidis@gmail.com>
2026-07-13 15:39:40 +0300

    Move EGL static analyser to engine plugin

--- plugins/org.eclipse.epsilon.egl.engine/src/org/eclipse/epsilon/egl/analyse/EglStaticAnalyser.java
+++ plugins/org.eclipse.epsilon.egl.engine/src/org/eclipse/epsilon/egl/analyse/EglStaticAnalyser.java
@@ -1,15 +1,10 @@
 package org.eclipse.epsilon.egl.analyse;
 
-import java.nio.charset.StandardCharsets;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.util.Collections;
 import java.util.List;
 
 import org.eclipse.epsilon.common.module.IModule;
 import org.eclipse.epsilon.common.module.ModuleMarker;
-import org.eclipse.epsilon.egl.EglModule;
 import org.eclipse.epsilon.egl.IEglModule;
 import org.eclipse.epsilon.egl.output.IOutputBuffer;
 import org.eclipse.epsilon.eol.IEolModule;
@@ -18,11 +13,13 @@ import org.eclipse.epsilon.eol.analyse.IModelFactory;
 import org.eclipse.epsilon.eol.staticanalyser.execute.context.Variable;
 import org.eclipse.epsilon.eol.staticanalyser.types.EolNativeType;
 
-
 public class EglStaticAnalyser extends EolStaticAnalyser {
 	
 	public EglStaticAnalyser(IModelFactory modelFactory) {
 		super(modelFactory);
+	}
+	
+	public EglStaticAnalyser() {
 	}
 	
 	@Override
@@ -44,32 +41,6 @@ public class EglStaticAnalyser extends EolStaticAnalyser {
 		super.postValidate();
 
 		return markers;
-	}
-	
-	public static void main(String[] args) throws Exception {
-		Path eglPath = Paths.get("test.egl");
-		if (!Files.exists(eglPath)) {
-			throw new IllegalStateException(
-				"Couldn't find test file at: " + eglPath.toAbsolutePath() + System.lineSeparator()
-					+ "Create it at: plugins/org.eclipse.epsilon.egl.staticanalyser/test.egl");
-		}
-
-		String content = new String(Files.readAllBytes(eglPath), StandardCharsets.UTF_8);
-		System.out.println("Loaded " + eglPath.toAbsolutePath());
-		System.out.println("File content: " + content);
-		
-		IEglModule module = new EglModule();
-		module.parse(content);
-		EglStaticAnalyser analyser = new EglStaticAnalyser(null);
-		List<ModuleMarker> markers = analyser.validate(module);
-		if (markers.isEmpty()) {
-			System.out.println("No problems found.");
-		} else {
-			System.out.println("Problems found:");
-			for (ModuleMarker marker : markers) {
-				System.out.println(marker);
-			}
-		}
 	}
 	
 }

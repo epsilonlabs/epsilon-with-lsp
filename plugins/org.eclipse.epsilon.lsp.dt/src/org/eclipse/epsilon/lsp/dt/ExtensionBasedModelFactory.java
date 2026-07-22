6ecd47326845652b4af062be1c2fd424b91060fd
Panagiotis Kourouklidis <panagiotis.kourouklidis@gmail.com>
2026-07-22 13:09:29 +0300

    Add "Unknown" model support to ExtensionBasedModelFactory

--- plugins/org.eclipse.epsilon.lsp.dt/src/org/eclipse/epsilon/lsp/dt/ExtensionBasedModelFactory.java
+++ plugins/org.eclipse.epsilon.lsp.dt/src/org/eclipse/epsilon/lsp/dt/ExtensionBasedModelFactory.java
@@ -4,11 +4,15 @@ import org.eclipse.core.runtime.CoreException;
 import org.eclipse.epsilon.common.dt.launching.extensions.ModelTypeExtension;
 import org.eclipse.epsilon.eol.analyse.IModelFactory;
 import org.eclipse.epsilon.eol.models.IModel;
+import org.eclipse.epsilon.eol.models.UnknownModel;
 
 public class ExtensionBasedModelFactory implements IModelFactory {
 
 	@Override
 	public IModel createModel(String driver) {
+		if ("Unknown".equals(driver)) {
+			return new UnknownModel();
+		}
 		try {
 			return ModelTypeExtension.forType(driver).createModel();
 		} catch (CoreException e) {

765856094980a4910631f95af1d1406da69af431
Panagiotis Kourouklidis <panagiotis.kourouklidis@gmail.com>
2026-07-22 10:42:29 +0300

    Use ExtensionBasedModelFactory inside Eclipse

--- plugins/org.eclipse.epsilon.lsp.dt/src/org/eclipse/epsilon/lsp/dt/ConnectionProviderSolution.java
+++ plugins/org.eclipse.epsilon.lsp.dt/src/org/eclipse/epsilon/lsp/dt/ConnectionProviderSolution.java
@@ -34,7 +34,7 @@ public class ConnectionProviderSolution implements StreamConnectionProvider {
 
 	private static EpsilonLanguageServer createLanguageServer() {
 		EpsilonLanguageServer languageServer = new EpsilonLanguageServer();
-		// TODO: make the server use the ExtensionBasedModelFactory
+		languageServer.setModelFactory(new ExtensionBasedModelFactory());
 		languageServer.addNativeTypeClassLoader(new ExtensionPointToolClassLoader());
 		return languageServer;
 	}

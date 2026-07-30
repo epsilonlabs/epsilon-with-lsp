410baecba7d2611bebf0ed732d90898b18d981ef
Panagiotis Kourouklidis <panagiotis.kourouklidis@gmail.com>
2026-07-30 15:14:48 +0300

    Fix CI

--- plugins/org.eclipse.epsilon.lsp/src/org/eclipse/epsilon/lsp/Analyser.java
+++ plugins/org.eclipse.epsilon.lsp/src/org/eclipse/epsilon/lsp/Analyser.java
@@ -280,20 +280,23 @@ public class Analyser {
 			return Collections.emptyList();
 		}
 
-		String code = readDocumentCode(fileUri);
-		if (code == null) {
+		final URI moduleUri;
+		try {
+			moduleUri = new URI(MapEntryRegistry.PROTOCOL, "", fileUri.getPath(), null);
+		} catch (URISyntaxException e) {
+			LOGGER.warning("Failed to create document symbol URI: " + e.getMessage());
 			return Collections.emptyList();
 		}
 
 		try {
-			module.parse(code, sourceFileFor(fileUri));
+			module.parse(moduleUri);
 		} catch (Exception e) {
 			// Parser recovery may still have produced declarations that are useful
 			// while the document is being edited.
 			LOGGER.warning("Failed to parse document while computing symbols: " + e.getMessage());
 		}
 
-		return DocumentSymbolExtractor.extract(module, code);
+		return DocumentSymbolExtractor.extract(module, readDocumentCode(fileUri));
 	}
 
 	private Location toLocation(ModuleElement element, URI fallbackUri) {
